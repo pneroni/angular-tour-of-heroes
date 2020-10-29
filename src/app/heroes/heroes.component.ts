d3685b89be7f3bfbcd99ab24468abe614eb97a50
Paolo Neroni <paolo.neroni@altran.it>
2020-10-29 14:53:51 +0100

    step 3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -17,4 +18,10 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
